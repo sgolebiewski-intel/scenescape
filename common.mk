b0664284d11a932c62d7e701b2bca636ed9a74d3
daddo-intel <derrick.addo@intel.com>
2025-06-12 22:25:35 -0700

    Update common makefile to add optional test target during build process

--- common.mk
+++ common.mk
@@ -28,7 +28,9 @@ build-image: Dockerfile
 	@{ \
 	    set -e; \
 	    set -o pipefail; \
-	    if env BUILDKIT_PROGRESS=plain docker build $(REBUILDFLAGS) \
+	    TARGET_ARG=""; \
+	    if [ -n "$(TARGET)" ]; then TARGET_ARG="--target $(TARGET)"; fi; \
+	    if env BUILDKIT_PROGRESS=plain docker build $(REBUILDFLAGS) $$TARGET_ARG \
 	        --build-arg RUNTIME_OS_IMAGE=$(RUNTIME_OS_IMAGE) \
 	        --build-arg http_proxy=$(http_proxy) \
 	        --build-arg https_proxy=$(https_proxy) \
