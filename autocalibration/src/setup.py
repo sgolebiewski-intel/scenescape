8fd1819a15bf9bea4652cbf3827e6c31a5ad23a4
daddo-intel <derrick.addo@intel.com>
2025-06-18 07:21:13 -0700

    Add test stage for controller, autocalibration and percebro dockerfiles

--- autocalibration/src/setup.py
+++ autocalibration/src/setup.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023-2024 Intel Corporation
+# Copyright (C) 2022-2023 Intel Corporation
 #
 # This software and the related documents are Intel copyrighted materials,
 # and your use of them is governed by the express license under which they
@@ -9,23 +9,16 @@
 # This software and the related documents are provided as is, with no express
 # or implied warranties, other than those that are expressly stated in the License.
 
-from threading import Lock
+from setuptools import setup, find_packages
+import os
 
-class FrameBuffer:
-  def __init__(self):
-    self.buffer = [None]*2
-    self.next_idx = 0
-    self.last_idx = 1
-    self.lock = Lock()
-    return
-
-  def addFrame(self, frame):
-    with self.lock:
-      self.buffer[self.next_idx] = frame
-      # Swap the indices
-      self.next_idx, self.last_idx = self.last_idx, self.next_idx
-    return
-
-  def getFrame(self):
-    with self.lock:
-      return self.buffer[self.last_idx]
+setup(
+    name='autocalibration',
+    package_dir={'': '.'},
+    packages=find_packages(),
+    python_requires='>=3.7',
+    license='Intel Confidential',
+    version='1.0.0',
+    author='Intel Corporation',
+    description='SceneScape core functionality',
+)
