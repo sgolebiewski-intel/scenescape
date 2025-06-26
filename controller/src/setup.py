a16d6f3e7189674f250ce65dfd2ea29687321d01
daddo-intel <derrick.addo@intel.com>
2025-06-26 15:56:34 -0700

    Remove specified packages from controller/src/setup.py

--- controller/src/setup.py
+++ controller/src/setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(
     name='controller',
     package_dir={'': '.'},
-    packages=find_packages(where='.', include=['controller', 'controller.*', 'tools', 'tools.*']),
+    packages=find_packages(),
     python_requires='>=3.7',
     license='Intel Confidential',
     version='1.0.0',
