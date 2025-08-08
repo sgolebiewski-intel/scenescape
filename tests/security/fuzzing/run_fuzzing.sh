7a816515395582cfad5f0b9141191192828ab944
daddo-intel <derrick.addo@intel.com>
2025-08-07 18:05:12 -0700

    Update restler_mode command

--- tests/security/fuzzing/run_fuzzing.sh
+++ tests/security/fuzzing/run_fuzzing.sh
@@ -14,4 +14,5 @@ auth_token=$(curl "https://web.scenescape.intel.com/api/v1/auth" -d "username=$a
 sed -i "s/##TOKEN##/$auth_token/" /tmp/token
 
 /RESTler/restler/Restler compile --api_spec fuzzing_openapi.yaml
-/RESTler/restler/Restler $restler_mode --time_budget $time_budget_hours --grammar_file Compile/grammar.py --dictionary_file Compile/dict.json --settings settings.json
+/RESTler/restler/Restler $restler_mode --time_budget $time_budget_hours \
+  --grammar_file Compile/grammar.py --dictionary_file Compile/dict.json --settings settings.json
