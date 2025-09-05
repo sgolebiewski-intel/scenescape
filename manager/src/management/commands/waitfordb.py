75db005ea3b0f71e5930d8d8f07d695709d05f9b
Derrick Addo <derrick.addo@intel.com>
2025-09-04 18:34:04 -0700

    Remove postgres installation from manager/Dockerfile

--- manager/src/management/commands/waitfordb.py
+++ manager/src/management/commands/waitfordb.py
@@ -1,13 +1,13 @@
 # SPDX-FileCopyrightText: (C) 2021 - 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-import psycopg2
 import time
 from scene_common import log
 from django.core.management.base import BaseCommand
 from django.conf import settings
 import manager.models
 from django.db.models.base import ModelBase
+from django.db import connection
 
 class Command(BaseCommand):
   def fullExceptionName(self, ex):
@@ -20,47 +20,50 @@ class Command(BaseCommand):
   def handle(self, *args, **options):
     while True:
       try:
-        conn = psycopg2.connect("dbname='" + settings.DATABASES['default']['NAME']
-                                + "' user='" + settings.DATABASES['default']['USER']
-                                + "' host='" + settings.DATABASES['default']['HOST']
-                                + "' password='" + settings.DATABASES['default']['PASSWORD']
-                                + "'")
-        tables_av = {}
-        # This first loop waits for the tables to exist.
-        # If one of the tables in the DB doesnt exist, it will end up retrying.
-        log.info("Waiting for tables")
-        for model, obj in manager.models.__dict__.items():
-          if isinstance( obj, ModelBase ):
-            data_av = obj.objects.all()
-            if model not in tables_av:
-              tables_av[model] = len(data_av)
+        print(settings.DATABASES['default']['NAME'])
+        print(settings.DATABASES['default']['USER'])
+        print(settings.DATABASES['default']['HOST'])
+        print(settings.DATABASES['default']['PASSWORD'])
 
-        # This next loop waits for tables to stabilize,
-        # Cant wait for them to be non-empty,
-        # since an empty DB is a valid usecase.
+        with connection.cursor() as cursor:
+          cursor.execute("SELECT 1;")  # simple test query
+
+        tables_av = {}
+        log.info("Waiting for tables")
+
+        # Check that tables exist
+        for model, obj in manager.models.__dict__.items():
+          if isinstance(obj, ModelBase):
+            try:
+              data_av = obj.objects.all()
+              if model not in tables_av:
+                tables_av[model] = len(data_av)
+            except Exception:
+              # table might not exist yet
+              log.info(f"Table for model {model} not ready yet")
+
         time.sleep(10)
         log.info("Waiting for data")
+
+        # Wait until table data stabilizes
         tables_updated = True
-        while tables_updated == True:
+        while tables_updated:
           tables_updated = False
           for model, obj in manager.models.__dict__.items():
-            if isinstance( obj, ModelBase ):
-              table_count = len(obj.objects.all())
-              if model not in tables_av or table_count != tables_av[model]:
-                tables_av[model] = table_count
-                tables_updated = True
+            if isinstance(obj, ModelBase):
+              try:
+                table_count = obj.objects.count()
+                if model not in tables_av or table_count != tables_av[model]:
+                  tables_av[model] = table_count
+                  tables_updated = True
+              except Exception:
+                log.info(f"Skipping model {model}, table not ready yet")
 
           if tables_updated:
             time.sleep(0.5)
-          else:
-            break
 
         log.info("Database ready")
         break
-
-      except psycopg2.OperationalError:
-        log.error("Server not online")
-        time.sleep(2)
 
       except Exception as ex:
         log.error("W4DB ERROR", self.fullExceptionName(ex), ex)
