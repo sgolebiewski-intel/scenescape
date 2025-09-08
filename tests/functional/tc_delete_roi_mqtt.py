bfa4f00a0333fa2bf3e2c6deb2e37210d3e252d4
dpitulax <99968922+dpitulax@users.noreply.github.com>
2025-09-08 11:57:52 +0200

    Update tc_delete_roi_mqtt.py

--- tests/functional/tc_delete_roi_mqtt.py
+++ tests/functional/tc_delete_roi_mqtt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# SPDX-FileCopyrightText: (C) 2022 - 2025 Intel Corporation
+# SPDX-FileCopyrightText: (C) 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 from tests.functional.common_scene_obj import SceneObjectMqtt
@@ -8,27 +8,27 @@ from tests.functional.common_scene_obj import SceneObjectMqtt
 TEST_NAME = "NEX-T10430"
 
 def runROIMqttDelete(self):
-    self.exitCode = 1
-    self.runSceneObjMqttInitialize()
-    try:
-        self.runSceneObjMqttPrepare()
-        self.runROIMqttExecute()
-        self.runROIMqttDelete()
-        passed_after_delete = self.runROIMqttVerifyNoEventsAfterDelete()
-        if passed_after_delete:
-            self.exitCode = 0
-    finally:
-        self.runSceneObjMqttFinally()
-    return
+  self.exitCode = 1
+  self.runSceneObjMqttInitialize()
+  try:
+    self.runSceneObjMqttPrepare()
+    self.runROIMqttExecute()
+    self.runROIMqttDelete()
+    passed_after_delete = self.runROIMqttVerifyNoEventsAfterDelete()
+    if passed_after_delete:
+      self.exitCode = 0
+  finally:
+    self.runSceneObjMqttFinally()
+  return
 
 def test_roi_delete(request, record_xml_attribute):
-    test = SceneObjectMqtt(TEST_NAME, request, record_xml_attribute)
-    runROIMqttDelete(test)
-    assert test.exitCode == 0
-    return
+  test = SceneObjectMqtt(TEST_NAME, request, record_xml_attribute)
+  runROIMqttDelete(test)
+  assert test.exitCode == 0
+  return
 
 def main():
-    return test_roi_delete(None, None)
+  return test_roi_delete(None, None)
 
 if __name__ == '__main__':
-    os._exit(main() or 0)
+  os._exit(main() or 0)
