86eec27582cd48e349cc2fabcb610cb5ea4ef8b7
Derrick Addo <derrick.addo@intel.com>
2026-04-30 20:55:58 -0400

    ITEP-87923: Validate user password and username API fields (#1335)

--- manager/src/manager/serializers.py
+++ manager/src/manager/serializers.py
@@ -830,6 +830,31 @@ class UserSerializer(NonNullSerializer):
   uid = serializers.CharField(source="pk", read_only=True)
   acls = PubSubACLSerializer(many=True, required=False)
 
+  def validate(self, attrs):
+    is_update = self.instance is not None
+
+    if not is_update:
+      username = attrs.get('username', None)
+      password = attrs.get('password', None)
+
+      if not username or not username.strip():
+        raise serializers.ValidationError({'username': ['This field is required.']})
+      if not password:
+        raise serializers.ValidationError({'password': ['This field is required.']})
+
+    else:
+      if 'username' in self.initial_data:
+        username = attrs.get('username', '')
+        if not username or not username.strip():
+          raise serializers.ValidationError({'username': ['This field may not be blank.']})
+
+      if 'password' in self.initial_data:
+        password = attrs.get('password', '')
+        if not password or not password.strip():
+          raise serializers.ValidationError({'password': ['This field may not be blank.']})
+
+    return super().validate(attrs)
+
   def create_update(self, validated_data, instance=None):
     is_update = instance is not None
 
